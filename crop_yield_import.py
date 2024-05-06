e5eeeee0f8bf1229cec956eb56884d8c89cdb7dd
lotzsi <simon.lotz@posteo.de>
2024-05-06 11:03:31 +0200

    import

--- crop_yield_import.py
+++ crop_yield_import.py
@@ -11,11 +11,23 @@ file_path = 'data/crop_yield/estat_apro_cpshr.tsv' # Crop production in EU stand
 shapefile_path = 'data/EU_regions/NUTS_RG_60M_2021_3035.shp/NUTS_RG_60M_2021_3035.shp'
 nuts_regions = gpd.read_file(shapefile_path)
 
+
 # Load the text file containing the NUTS region codes
-crop_yield = pd.read_csv(file_path, delimiter='\t')  # Adjust delimiter as per your file format
+crop_yield = pd.read_csv(file_path, sep='\t')
+
+# Extract the desired columns and remove the unwanted characters
+crop_yield['geo'] = df['geo\\TIME_PERIOD'].str.split(',').str[-1]
+crop_yield['strucpro'] = df['geo\\TIME_PERIOD'].str.split(',').str[-2]
+crop_yield['crops'] = df['geo\\TIME_PERIOD'].str.split(',').str[-3]
+crop_yield['freq'] = df['geo\\TIME_PERIOD'].str.split(',').str[-4]
+
+# Drop the original column
+crop_yield.drop(columns=['geo\\TIME_PERIOD'], inplace=True)
+
+print(crop_yield.head())
 
 # Perform the join based on the common column (NUTS region code)
-joined_data = pd.merge(nuts_regions, crop_yield, on='NUTS_CODE', how='inner')
+joined_data = pd.merge(nuts_regions, crop_yield, on='NUTS_ID', how='inner')
 
 # Now `joined_data` contains both the geometries from the shapefile and the data from the text file joined together
 print(joined_data.head())
