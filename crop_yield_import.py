9145e2b6f862eb00b49c9307d99c687660958843
lotzsi <simon.lotz@posteo.de>
2024-05-06 10:12:16 +0200

    readme

---
+++ crop_yield_import.py
@@ -0,0 +1,10 @@
+#crop yield import
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_path = 'data/crop_yield/estat_apro_cpsh1.tsv' # Crop production in EU standard humidity (apro_cpsh1)
+file_path2 = 'data/crop_yield/estat_apro_cpshr.tsv' # Crop production in EU standard humidity by NUTS 2 regions (apro_cpshr)
+
+with open 
