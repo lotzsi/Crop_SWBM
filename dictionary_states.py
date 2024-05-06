b6bed5af82c1be0cae90200cc36a762148a74a9d
piamialia <pia-marie.m@gmx.net>
2024-05-06 11:23:58 +0200

    added dictionary

---
+++ dictionary_states.py
@@ -0,0 +1,18 @@
+state = {
+    'DE1': 'Baden-Württemberg',
+    'DE2': 'Bavaria',
+    'DE3': 'Berlin',
+    'DE4': 'Brandenburg',
+    'DE5': 'Bremen',
+    'DE6': 'Hamburg',
+    'DE7': 'Hessen',
+    'DE8': 'Mecklenburg-Vorpommern',
+    'DE9': 'Lower Saxony',
+    'DEA': 'North Rhine-Westphalia',
+    'DEB': 'Rhineland-Palatinate',
+    'DEC': 'Saarland',
+    'DED': 'Saxony',
+    'DEE': 'Saxony-Anhalt',
+    'DEF': 'Schleswig-Holstein',
+    'DEG': 'Thuringia'
+}
