2e84d3b63cbb5224715ce4be0d769fcd31c5e361
lotzsi <simon.lotz@posteo.de>
2024-05-06 14:08:05 +0200

    yield

--- crop_yield_import.py
+++ crop_yield_import.py
@@ -15,7 +15,7 @@ nuts_regions = gpd.read_file(shapefile_path)
 nuts_regions = nuts_regions[['NUTS_ID', 'geometry']]
 
 # Load the text file containing the NUTS region codes
-crop_yield = pd.read_csv(file_path, sep='\t')
+crop_yield = pd.read_csv(file_path, sep='\t', na_values=':')
 
 # Extract the desired columns and remove the unwanted characters
 crop_yield['NUTS_ID'] = crop_yield['freq,crops,strucpro,geo\TIME_PERIOD'].str.split(',').str[-1]
@@ -25,6 +25,27 @@ crop_yield['freq'] = crop_yield['freq,crops,strucpro,geo\TIME_PERIOD'].str.split
 
 # Drop the original column
 crop_yield.drop(columns=['freq,crops,strucpro,geo\TIME_PERIOD'], inplace=True)
+print(crop_yield.head())
+
+# Select only the year columns and the "NUTS_ID" column
+year_columns = [col for col in crop_yield.columns if col[:4].isdigit() and 2000 <= int(col[:4]) <= 2024]
+filtered_data = crop_yield[['NUTS_ID'] + year_columns]
+
+# Group the DataFrame by "NUTS_ID" and sum up the values for year columns
+summed_data = filtered_data.groupby('NUTS_ID').sum().reset_index()
+
+print(summed_data.head())
+summed_data.to_csv('summed.csv', index = False)
+
+# Select only the year columns and the "NUTS_ID" column
+#year_columns = [str(year) for year in range(2000, 2025)]
+#filtered_data = crop_yield[['NUTS_ID'] + year_columns]
+
+# Group the DataFrame by "NUTS_ID" and sum up the values for year columns
+#summed_data = filtered_data.groupby('NUTS_ID', as_index=False)[year_columns].sum()
+
+#print(summed_data.head())
+
 # Perform the join based on the common column (NUTS region code)
 joined_data = pd.merge(nuts_regions, crop_yield, on='NUTS_ID', how='inner')
 filter_DE = joined_data[joined_data["NUTS_ID"].str.startswith("DE")]
@@ -40,4 +61,3 @@ nuts_ids_to_keep = list(ds.states.keys())
 filtered_data = joined_data[joined_data["NUTS_ID"].isin(nuts_ids_to_keep)]
 filtered_data = filtered_data.drop_duplicates()
 
-#filtered_data.to_csv('filtered_data.csv', index=False)
